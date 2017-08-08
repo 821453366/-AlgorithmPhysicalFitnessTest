a416eedac814293f578d5ad6604969c1fc64b1a3
821453366 <821453366@qq.com>
2017-08-08 14:40:57 +0800

    提交第一题

---
+++ PrimeFactors/primeFactors.js
@@ -0,0 +1,21 @@
+function printPrimeFactors(number) {
+    let primeFactors = "";
+
+    primeFactors += (number + "=");
+    for (var i = 2; i <= number; i++) {
+        while (number != i) {
+            if (number % i == 0) {
+                primeFactors += i;
+                number = number / i;
+            } else {
+                break;
+            }
+        }
+    }
+    primeFactors += number;
+
+    console.log(primeFactors);
+}
+
+printPrimeFactors(90);
+
