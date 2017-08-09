635b0f6d952c91281cb456efa6bf01b912b2367a
821453366 <821453366@qq.com>
2017-08-09 14:46:31 +0800

    修改

--- FibonacciSeries/FibonacciSeries.js
+++ FibonacciSeries/FibonacciSeries.js
@@ -5,9 +5,8 @@ function fibonacciSeries(num) {
     for (let i = 2; i < number;i++) {
         fibonacciSeriesNumber.push(fibonacciSeriesNumber[i-1]+fibonacciSeriesNumber[i-2]);
     }
-    console.log(`前`+num+`个数的斐波纳契数列：`+fibonacciSeriesNumber);
 
-    return fibonacciSeriesNumber.pop();
+    return fibonacciSeriesNumber[num-1];
 }
 
 console.log(fibonacciSeries(10));
