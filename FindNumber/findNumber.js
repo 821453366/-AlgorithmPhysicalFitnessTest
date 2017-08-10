e8ce8e41ef4e2d763f6080982fb641ee43ce2d24
821453366 <821453366@qq.com>
2017-08-10 14:35:04 +0800

    重构代码

--- FindNumber/findNumber.js
+++ FindNumber/findNumber.js
@@ -1,16 +1,19 @@
 function findNumber(numbers) {
     let numberArray = numbers;
-    let result;
+    let result =0;
 
     numberArray.forEach(item => {
-        if (numberArray.filter(element => element === item).length === 1) {
+        if (isOneNumber(item, numberArray))
             result = item;
-        }
     });
 
     return result;
 }
 
+function isOneNumber(item, numberArray) {
+    return numberArray.filter(element => element === item).length === 1;
+}
+
 let numbers = [17, 12, 25, 9, 15, 30, 7, 32, 12, 25, 15, 7, 17, 32, 26, 26, 30];
-console.log("数组："+numbers);
+console.log("数组：" + numbers);
 console.log(findNumber(numbers));
