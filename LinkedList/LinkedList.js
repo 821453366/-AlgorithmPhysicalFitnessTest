0f791805acfdea4cc211dd05c851f35c25ded792
821453366 <821453366@qq.com>
2017-08-11 14:14:08 +0800

    修改

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -2,8 +2,9 @@ function linkedList(str, val) {
     let linked = str;
     let linkedArray = linked.split('->')
 
-    return linkedArray.filter(item => item !== val).join('->');
+    return linkedArray.filter(item => parseInt(item) !== val).join('->');
 }
 
 let str = '1->2->3->3->4->5->3';
-console.log(linkedList(str, '3'));
+let val = 3
+console.log(linkedList(str, val));
