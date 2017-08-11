9a364b776a41025493ff5bb42ea8b5b56a35981f
821453366 <821453366@qq.com>
2017-08-11 14:15:20 +0800

    重构

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,10 +1,10 @@
 function linkedList(str, val) {
     let linked = str;
-    let linkedArray = linked.split('->')
+    let linkedArray = linked.split('->');
 
     return linkedArray.filter(item => parseInt(item) !== val).join('->');
 }
 
 let str = '1->2->3->3->4->5->3';
-let val = 3
+let val = 3;
 console.log(linkedList(str, val));
