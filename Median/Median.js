aa5dd723173c80abddaa1c74444230eb1029d97c
821453366 <821453366@qq.com>
2017-08-13 16:05:10 +0800

    代码重构

--- Median/Median.js
+++ Median/Median.js
@@ -1,21 +1,13 @@
-function median(num) {
-    let numbers = num;
-    let medianNum = 0;
+function median(array) {
+    let numbers = array;
 
     numbers.sort((a, b) => a - b);
-    numbers.forEach((item, index) => {
-        if (numbers.length % 2 === 0) {
-            if ((index + 1) === numbers.length / 2 || (index + 1) === (numbers.length / 2) + 1)
-                medianNum += item;
-        } else {
-            if ((index + 1) === Math.ceil(numbers.length / 2))
-                medianNum = item;
-        }
-    });
+    if (numbers.length % 2 === 0) {
+        return (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2;
+    }
 
-    if (numbers.length % 2 === 0)
-        return medianNum / 2;
-    else
-        return medianNum;
+    return numbers[Math.floor(numbers.length / 2)];
 }
+
 console.log(median([9, 6, 7, 8, 10]));
+console.log(median([9, 6, 7, 8]));
