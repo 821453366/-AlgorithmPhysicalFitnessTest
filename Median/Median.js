43610e0fe90cf225f7bb0f730036e22e9d57d21c
821453366 <821453366@qq.com>
2017-08-13 16:07:17 +0800

    重构

--- Median/Median.js
+++ Median/Median.js
@@ -1,13 +1,17 @@
+
 function median(array) {
     let numbers = array;
 
     numbers.sort((a, b) => a - b);
-    if (numbers.length % 2 === 0) {
+    if (isEvenNumbers(numbers)) {
         return (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2;
     }
 
     return numbers[Math.floor(numbers.length / 2)];
 }
 
+function isEvenNumbers(numbers) {
+    return numbers.length % 2 === 0;
+}
 console.log(median([9, 6, 7, 8, 10]));
 console.log(median([9, 6, 7, 8]));
