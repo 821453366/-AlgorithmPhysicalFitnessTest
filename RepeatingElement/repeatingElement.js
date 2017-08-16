d4b66f11e2def208adeed459d4acaf3486d9e675
821453366 <821453366@qq.com>
2017-08-16 14:55:38 +0800

    重构

--- RepeatingElement/repeatingElement.js
+++ RepeatingElement/repeatingElement.js
@@ -1,24 +1,9 @@
 function repeatingElement(array) {
     let setArray = Array.from(new Set(array));
-    let result = [];
-    let data = [];
 
-    setArray.forEach(item => {
-       result.push({key:item,count:0});
-    });
-    array.forEach(item => {
-        result.forEach(data => {
-            if (item === data.key)
-                data.count++;
-        })
-    });
-     result.forEach(item => {
-        if(item.count>1)
-            data.push(item.key);
-    });
+    return setArray.filter(item => (array.filter(data => item === data).length) > 1);
 
-     return data;
 }
 
 let arr = ['a', 'ab', 'ed', 'da', 'a', 'ed', 'b'];
-console.log("['a', 'ab', 'ed', 'da', 'a', 'ed', 'b']===>  "+repeatingElement(arr))
+console.log("['a', 'ab', 'ed', 'da', 'a', 'ed', 'b']===>  " + repeatingElement(arr))
