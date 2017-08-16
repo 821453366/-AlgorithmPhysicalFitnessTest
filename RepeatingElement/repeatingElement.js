ef6cc263a545d6814528f1ea8f09d2b6e66a7936
821453366 <821453366@qq.com>
2017-08-16 14:56:41 +0800

    重构

--- RepeatingElement/repeatingElement.js
+++ RepeatingElement/repeatingElement.js
@@ -2,7 +2,6 @@ function repeatingElement(array) {
     let setArray = Array.from(new Set(array));
 
     return setArray.filter(item => (array.filter(data => item === data).length) > 1);
-
 }
 
 let arr = ['a', 'ab', 'ed', 'da', 'a', 'ed', 'b'];
