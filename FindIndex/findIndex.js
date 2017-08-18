dc792d8031dd412d7c2605d59d444f4f0de0bf37
821453366 <821453366@qq.com>
2017-08-18 14:27:23 +0800

    重构

--- FindIndex/findIndex.js
+++ FindIndex/findIndex.js
@@ -1,14 +1,13 @@
 function findIndex(arr, item) {
     let result =[];
 
-     arr.forEach((data, index) => {
+    arr.reduce((accumu,data,index) => {
         if(data === item)
             result.push(index);
-
-    });
+    },0);
 
      return result;
 }
-let arr = [1, 3, 7, 1, 4,];
+let arr = [1, 3, 7, 1, 4];
 let item = 1;
-console.log("[1, 3, 7, 1, 4,]中1元素的下标===>"+findIndex(arr, item));
+console.log("[1, 3, 7, 1, 4]中1元素的下标===>"+findIndex(arr, item));
